5d52db8856880b7d1b127fbeba4e018a04fd01ed
Hendrik Moree <hendrik@seecr.nl>
2015-10-12 09:42:38 +0200

    TJ/HM: Copied CrashDetect from Zoekplatform

---
+++ meresco/distributed/crashdetect.py
@@ -0,0 +1,70 @@
+## begin license ##
+#
+# "NBC+" also known as "ZP (ZoekPlatform)" is
+#  a project of the Koninklijke Bibliotheek
+#  and provides a search service for all public
+#  libraries in the Netherlands.
+#
+# Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
+# Copyright (C) 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# This file is part of "NBC+ (Zoekplatform BNL)"
+#
+# "NBC+ (Zoekplatform BNL)" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "NBC+ (Zoekplatform BNL)" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "NBC+ (Zoekplatform BNL)"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
+from os import remove, rename
+from os.path import isfile, join
+from time import time
+
+from meresco.distributed.servicestate import ERROR
+
+from seecr.zulutime import ZuluTime
+
+
+class CrashDetect(object):
+    def __init__(self, stateDir, severity=ERROR):
+        self._filePath = join(stateDir, 'lastSafePoint')
+        self._severity = severity
+        self.didCrash = isfile(self._filePath)
+        self._markSafePoint()
+
+    def serviceData(self, dataDict):
+        if self.didCrash:
+            errors = dataDict.get(self._severity, 0)
+            dataDict[self._severity] = errors + 1
+
+    def errorMessage(self):
+        return 'Data considered incomplete after crash [last safe point: %s].' % ZuluTime.parseEpoch(self.lastSafePoint()).zulu() if self.didCrash else None
+
+    def commit(self):
+        self._markSafePoint()
+
+    def handleShutdown(self):
+        if not self.didCrash:
+            remove(self._filePath)
+
+    def lastSafePoint(self):
+        return float(open(self._filePath).read())
+
+    def _markSafePoint(self):
+        if not self.didCrash:
+            with open(self._filePath + '~', 'w') as f:
+                f.write("%f" % self._time())
+            rename(self._filePath + '~', self._filePath)
+
+    def _time(self):
+        return time()
---
+++ test/crashdetecttest.py
@@ -0,0 +1,101 @@
+## begin license ##
+#
+# "Meresco Distributed" has components for group management based on "Meresco Components."
+#
+# Copyright (C) 2015 Drents Archief http://www.drentsarchief.nl
+# Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
+# Copyright (C) 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# This file is part of "Meresco Distributed"
+#
+# "Meresco Distributed" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "Meresco Distributed" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Distributed"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
+from os import remove
+from os.path import isfile
+
+from seecr.test import SeecrTestCase
+
+from meresco.distributed.servicestate import WARNING
+from meresco.distributed.crashdetect import CrashDetect
+
+
+class CrashDetectTest(SeecrTestCase):
+    def testNormalShutdown(self):
+        cd = CrashDetect(self.tempdir)
+        self.assertFalse(cd.didCrash)
+        self.assertTrue(isfile(cd._filePath))
+        cd.handleShutdown()
+        self.assertFalse(isfile(cd._filePath))
+        cd = CrashDetect(self.tempdir)
+        self.assertFalse(cd.didCrash)
+        dataDict = {}
+        cd.serviceData(dataDict)
+        self.assertEquals({}, dataDict)
+
+    def testDidNotShutdownProperly(self):
+        cd = CrashDetect(self.tempdir)
+        cd = CrashDetect(self.tempdir) # reopen
+        self.assertTrue(cd.didCrash)
+        self.assertTrue(cd.errorMessage().startswith('Data considered incomplete after crash [last safe point: '), cd.errorMessage())
+        dataDict = {}
+        cd.serviceData(dataDict)
+        self.assertEquals({'errors': 1}, dataDict)
+        cd.handleShutdown()
+        filePath = cd._filePath
+        cd = None
+        remove(filePath)  # manual reset
+        cd = CrashDetect(self.tempdir)
+        self.assertFalse(cd.didCrash)
+        dataDict = {}
+        cd.serviceData(dataDict)
+        self.assertEquals({}, dataDict)
+
+    def testShutdownAfterRestartAfterCrashKeepsErrorState(self):
+        cd = CrashDetect(self.tempdir)
+        cd = CrashDetect(self.tempdir) # reopen
+        self.assertTrue(cd.didCrash)
+        errorMessage = cd.errorMessage()
+        self.assertTrue(errorMessage.startswith('Data considered incomplete after crash [last safe point: '), errorMessage)
+        cd.handleShutdown()
+        cd = CrashDetect(self.tempdir)
+        self.assertTrue(cd.didCrash)
+        self.assertEquals(errorMessage, cd.errorMessage())
+
+    def testKeepEarliestCrashTimestamp(self):
+        cd = CrashDetect(self.tempdir)
+        lsc = cd.lastSafePoint()
+        cd = CrashDetect(self.tempdir)
+        self.assertTrue(cd.didCrash)
+        self.assertEquals(lsc, cd.lastSafePoint())
+        self.assertTrue(cd.errorMessage().startswith('Data considered incomplete after crash [last safe point: '), cd.errorMessage())
+        cd = CrashDetect(self.tempdir)
+        self.assertTrue(cd.didCrash)
+        self.assertEquals(lsc, cd.lastSafePoint())
+
+    def testServiceDataIncrement(self):
+        dataDict = {'errors': 1}
+        cd = CrashDetect(self.tempdir)
+        cd = CrashDetect(self.tempdir)
+        cd.serviceData(dataDict)
+        self.assertEquals({'errors': 2}, dataDict)
+
+    def testServiceDataWarningSeverity(self):
+        dataDict = {'errors': 1}
+        cd = CrashDetect(self.tempdir, severity=WARNING)
+        cd = CrashDetect(self.tempdir, severity=WARNING)
+        cd.serviceData(dataDict)
+        self.assertEquals({'errors': 1, 'warnings': 1}, dataDict)
