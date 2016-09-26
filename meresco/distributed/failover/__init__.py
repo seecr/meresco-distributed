89093b40f6136a2c6c20e1fc7d068a44c02e6dfb
Thijs Janssen <thijs@seecr.nl>
2016-09-26 19:53:54 +0200

    TJ: new style nginxconfig

--- meresco/distributed/failover/__init__.py
+++ meresco/distributed/failover/__init__.py
@@ -1,33 +1,31 @@
 ## begin license ##
 #
-# "NBC+" also known as "ZP (ZoekPlatform)" is
-#  a project of the Koninklijke Bibliotheek
-#  and provides a search service for all public
-#  libraries in the Netherlands.
-# This package provides loadbalancer scripts
+# "Meresco Distributed" has components for group management based on "Meresco Components."
 #
 # Copyright (C) 2012-2016 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2012-2014 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
 # Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
 # Copyright (C) 2016 Drents Archief http://www.drentsarchief.nl
+# Copyright (C) 2016 SURFmarket https://surf.nl
 #
-# This file is part of "NBC+ (Zoekplatform BNL) Loadbalancer"
+# This file is part of "Meresco Distributed"
 #
-# "NBC+ (Zoekplatform BNL) Loadbalancer" is free software; you can redistribute it and/or modify
+# "Meresco Distributed" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "NBC+ (Zoekplatform BNL) Loadbalancer" is distributed in the hope that it will be useful,
+# "Meresco Distributed" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "NBC+ (Zoekplatform BNL) Loadbalancer"; if not, write to the Free Software
+# along with "Meresco Distributed"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
 
 VERSION = '$Version: 1.0.x$'[9:-1].strip()
 from nginxconfig import NginxConfig
+from nginxconfigng import NginxConfigNG
